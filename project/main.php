75d412a2afcd347caddaaa3a1043fadbefbd4ad0
Nos4h <155986820+Nos4h@users.noreply.github.com>
2024-07-14 13:11:04 +0300

    Add files via upload

---
+++ project/main.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'test';
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+  
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+//     $conn->close();
+
+   echo "hello";
+    ?>
+    </body>
+</html>
